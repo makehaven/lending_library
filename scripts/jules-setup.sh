7459bac0dab3713d4c8cae3700cff6e6d7ee7b46
J.R. Logan <johnrichardlogan@gmail.com>
2025-08-27 02:39:08 -0400

    Add gitpod init script

--- scripts/jules-setup.sh
+++ scripts/jules-setup.sh
@@ -1,3 +1,4 @@
+# scripts/gitpod-init.sh
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -8,7 +9,7 @@ sleep 15
 echo "--- Install tools + Composer ---"
 docker compose exec -T drupal bash -lc 'apt-get update && apt-get install -y curl git unzip default-mysql-client'
 docker compose exec -T drupal bash -lc 'printf "sendmail_path = /bin/true\n" > /usr/local/etc/php/conf.d/mail.ini'
-docker compose exec -T drupal bash -lc 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && composer --version'
+docker compose exec -T drupal bash -lc 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer'
 
 echo "--- Create Drupal project ---"
 docker compose exec -T drupal bash -lc 'mkdir -p /opt/drupal && [ -f /opt/drupal/composer.json ] || composer create-project drupal/recommended-project:^10.3 /opt/drupal'
@@ -18,16 +19,16 @@ echo "--- Require Drush + ECK ---"
 docker compose exec -T drupal bash -lc 'cd /opt/drupal && composer require drush/drush:^13 drupal/eck:^2 -W'
 
 echo "--- Install Drupal ---"
-/usr/bin/env docker compose exec -T drupal bash -lc "/opt/drupal/vendor/bin/drush si standard \
+docker compose exec -T drupal bash -lc "/opt/drupal/vendor/bin/drush si standard \
   --db-url='mysql://user:pass@db:3306/drupal' \
   --site-name='Gitpod Drupal' \
   --account-name=admin --account-pass=admin -y"
 
 echo "--- Enable ECK only ---"
-/usr/bin/env docker compose exec -T drupal bash -lc '/opt/drupal/vendor/bin/drush en eck -y'
+docker compose exec -T drupal bash -lc '/opt/drupal/vendor/bin/drush en eck -y'
 
 echo "--- Dev error verbosity ---"
-/usr/bin/env docker compose exec -T drupal bash -lc "cat <<'PHP' >> /opt/drupal/web/sites/default/settings.php
+docker compose exec -T drupal bash -lc "cat <<'PHP' >> /opt/drupal/web/sites/default/settings.php
 \$config['system.logging']['error_level'] = 'verbose';
 \$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 ini_set('display_errors', TRUE);
@@ -35,7 +36,7 @@ ini_set('display_startup_errors', TRUE);
 error_reporting(E_ALL);
 \$settings['rebuild_access'] = TRUE;
 PHP"
-/usr/bin/env docker compose exec -T drupal bash -lc "cat <<'YML' > /opt/drupal/web/sites/development.services.yml
+docker compose exec -T drupal bash -lc "cat <<'YML' > /opt/drupal/web/sites/development.services.yml
 parameters:
   http.response.debug_cacheability_headers: true
   twig.config:
@@ -45,6 +46,6 @@ parameters:
 YML"
 
 echo "--- Clear caches ---"
-/usr/bin/env docker compose exec -T drupal bash -lc '/opt/drupal/vendor/bin/drush cr'
+docker compose exec -T drupal bash -lc '/opt/drupal/vendor/bin/drush cr'
 
 echo "--- Done ---"
